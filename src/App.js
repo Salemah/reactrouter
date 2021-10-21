79b9e454cb4735b9e10af8ecabb900a560dad640
Salemkd <85119912+Salemah@users.noreply.github.com>
2021-10-21 23:43:46 +0600

    data added

--- src/App.js
+++ src/App.js
@@ -3,28 +3,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Card from './Component/Card';
 import { ButtonGroup, Button } from 'react-bootstrap';
+import { useEffect } from 'react';
 
 function App() {
-  const item = [{
-    name:'tanvir',description:'loremdhcbhdcbhdcjbjcbjbcjbbcj',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Shaheed_Minar.JPG/240px-Shaheed_Minar.JPG'
-  },{
-    name:'secdnd',description:'loremdhcbhdcbhdcjbjcbjbcjbbcj',img:'https://tourhobe.com/storage/packages/AfTaGKDJcXIrYiik2flJEA41UlJEXi2nzJap9ppo.jpg'
-  },{
-    name:'third',description:'loremdhcbhdcbhdcjbjcbjbcjbbcj',img:'https://tourhobe.com/storage/packages/AfTaGKDJcXIrYiik2flJEA41UlJEXi2nzJap9ppo.jpg'
-  },{
-    name:'fourth',description:'loremdhcbhdcbhdcjbjcbjbcjbbcj',img:'https://tourhobe.com/storage/packages/AfTaGKDJcXIrYiik2flJEA41UlJEXi2nzJap9ppo.jpg'
-  },
-]
+ 
+useEffect(()=>{
+  fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-09-21&sortBy=publishedAt&apiKey=ea06908fc5c24c019c765861c6eb80f0')
+  .then(res =>res.json())
+  .then(data =>console.log(data))
+},[])
   return (
     <div className="row row-cols-1 row-cols-md-4 g-4">
     <Button varient="primary">click</Button>
-      <Button varient="primary" >click</Button>
-      {/* {
-      item.map(product => <Card  item={product}/>)
-      } */}
+     
       
     </div>
   );
 }
 
 export default App;
+// const item = [{
+//   name:'tanvir',description:'loremdhcbhdcbhdcjbjcbjbcjbbcj',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Shaheed_Minar.JPG/240px-Shaheed_Minar.JPG'
+// },{
+//   name:'secdnd',description:'loremdhcbhdcbhdcjbjcbjbcjbbcj',img:'https://tourhobe.com/storage/packages/AfTaGKDJcXIrYiik2flJEA41UlJEXi2nzJap9ppo.jpg'
+// },{
+//   name:'third',description:'loremdhcbhdcbhdcjbjcbjbcjbbcj',img:'https://tourhobe.com/storage/packages/AfTaGKDJcXIrYiik2flJEA41UlJEXi2nzJap9ppo.jpg'
+// },{
+//   name:'fourth',description:'loremdhcbhdcbhdcjbjcbjbcjbbcj',img:'https://tourhobe.com/storage/packages/AfTaGKDJcXIrYiik2flJEA41UlJEXi2nzJap9ppo.jpg'
+// },
+// ]
